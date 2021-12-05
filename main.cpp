255e91d1e921cc9fd356366b3588df7095f9c6d9
ChelseaOwiti <chelseaowiti@gmail.com>
2021-12-05 18:47:56 +0300

    View port and projection matrix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <GL/gl.h>
 #include <GL/glut.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+void display_callback();
+void reshape_callback(int, int );
+void init(){
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+
+}
+
+int main( int argc, char  **argv) {
+    //initialise glut and display mode and window size
+    glutInit(&argc,argv);
+    //double buffer window
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("KENYAN SNAKE");
+    glutDisplayFunc(display_callback);
+    glutReshapeFunc(reshape_callback);
+    init();
+    glutMainLoop();
+
     return 0;
+}
+
+void display_callback()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+//used to set viewport
+void reshape_callback(int w, int h){
+    glViewport(0,0,(GLsizei)w,(GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 40.0, 0.0, 40.0, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
 }
